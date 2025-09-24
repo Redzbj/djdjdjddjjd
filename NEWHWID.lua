63df5cd367083da5f99dfe20c0d7769d2779258f
Redzbj <938338551@qq.com>
2025-09-24 15:41:20 +0800

    Add files via upload

---
+++ NEWHWID.lua
@@ -0,0 +1,6 @@
+local hwid = {
+    ["97fe20ab-df0a-4b23-ac9d-530da7eae67f"] = "DSWAP"--山风
+    ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa"--白子
+    ["058b7912-b536-4b53-a6ec-816294e96c5f"] = "12311211"--制冰机
+}
+return hwid
