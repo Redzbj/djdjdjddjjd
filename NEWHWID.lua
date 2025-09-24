ae803f54df0a8eb3c9479e0b37b0a51e6963386e
Redzbj <938338551@qq.com>
2025-09-24 18:57:44 +0800

    Update NEWHWID.lua

--- NEWHWID.lua
+++ NEWHWID.lua
@@ -1,7 +1,7 @@
 local hwid = {
     ["97fe20ab-df0a-4b23-ac9d-530da7eae67f"] = "DSWAP",
     ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa",
-    ["058b7912-b536-4b53-a6ec-816294e96c5f"] = "12311211",
+    ["60156fe5-e7a0-4e67-a7df-1c8d4d224e86"] = "12311211",
     ["6cb171c8-0356-4fc6-98da-e036ccb1d5f8"] = "hithere",
 }
 return hwid
