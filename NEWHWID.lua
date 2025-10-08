10b6eb54263b3048912aead2d092dd693d5557d9
Redzbj <938338551@qq.com>
2025-10-08 19:06:01 +0800

    Update NEWHWID.lua

--- NEWHWID.lua
+++ NEWHWID.lua
@@ -3,5 +3,6 @@ local hwid = {
     ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa",
     ["60156fe5-e7a0-4e67-a7df-1c8d4d224e86"] = "12311211",
     ["6cb171c8-0356-4fc6-98da-e036ccb1d5f8"] = "hithere",
+    ["04ee598b-0382-4388-a625-6667b9c9754a"] = "zzr3213",
 }
 return hwid
