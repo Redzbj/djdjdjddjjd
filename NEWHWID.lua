701a4c15fa3524e770baf0143f50609e6751b359
Redzbj <938338551@qq.com>
2025-11-12 16:48:24 +0800

    Update NEWHWID.lua

--- NEWHWID.lua
+++ NEWHWID.lua
@@ -2,6 +2,7 @@ local hwid = {
     ["97fe20ab-df0a-4b23-ac9d-530da7eae67f"] = "DSWAP",--山风
     ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa",--白子
     ["058b7912-b536-4b53-a6ec-816294e96c5f"] = "12311211",--制冰机
-    ["6cb171c8-0356-4fc6-98da-e036ccb1d5f8"] = "hithere"--404
+    ["6cb171c8-0356-4fc6-98da-e036ccb1d5f8"] = "hithere",--404
+    ["5ad1c94f-2e07-4533-85ec-8235eacd156e"] = "a560727003"--kub
 }
 return hwid
