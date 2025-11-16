277b38680479696852285a112a2178f76cbb622d
Redzbj <938338551@qq.com>
2025-11-16 21:03:14 +0800

    Fix syntax by adding missing comma in NEWHWID.lua

--- NEWHWID.lua
+++ NEWHWID.lua
@@ -3,6 +3,7 @@ local hwid = {
     ["9864d5d8-4ff1-4ae2-8c28-9b8c29553bf4"] = "Shirokoawa",--白子
     ["60156fe5-e7a0-4e67-a7df-1c8d4d224e86"] = "12311211",--制冰机
     ["6cb171c8-0356-4fc6-98da-e036ccb1d5f8"] = "hithere",--404
-    ["5ad1c94f-2e07-4533-85ec-8235eacd156e"] = "a560727003"--kub
+    ["5ad1c94f-2e07-4533-85ec-8235eacd156e"] = "a560727003",--kub
+    ["0ca56f95-6a0d-4582-8031-91226f4c8fca"] = "2140623758"
 }
 return hwid
